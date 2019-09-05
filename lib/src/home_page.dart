7cff87e9fc7f124dc5ebb4c55d679edff5305172
Cesar Murillo <namiayumis@gmail.com>
2019-09-05 16:36:45 +0900

    Bottom NavigatonBar

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/direcciones_page.dart';
+import 'package:qrreaderapp/src/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola HOme'),
-      ),
+      body: _callpage(currentIndex),
+      bottomNavigationBar: _crearBottomNavigationBar(),
+    );
+  }
+
+  Widget _callpage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+    }
+  }
+
+  Widget _crearBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.brightness_5),
+          title: Text('Direcciones'),
+        ),
+      ],
     );
   }
 }
