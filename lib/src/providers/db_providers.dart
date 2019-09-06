08928e79fe55668534660f2843a915c99eee9340
Cesar Murillo <namiayumis@gmail.com>
2019-09-06 12:08:18 +0900

    create table database

--- lib/src/providers/db_providers.dart
+++ lib/src/providers/db_providers.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -13,5 +16,18 @@ class DBProvider {
     return _database;
   }
 
-  initDB() async {}
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('CREATE TABLE Scans ('
+          ' id INTEGER PRIMARY KEY,'
+          ' tipo TEXT,'
+          ' valor TEXT'
+          ')');
+    });
+  }
 }
