3421628b9f8918f24ea0829e8ff850d407a33c11
Cesar Murillo <namiayumis@gmail.com>
2019-09-06 12:38:22 +0900

    crear registros

--- lib/src/providers/db_providers.dart
+++ lib/src/providers/db_providers.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:qrreaderapp/src/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -29,5 +31,20 @@ class DBProvider {
           ' valor TEXT'
           ')');
     });
+  }
+
+  // CREAR Registros
+  nuevoScanRaw(ScanModel nuevoScan) async {
+    final db = await database;
+
+    final res = await db.rawInsert("INSERT Into Scans (id, tipo, valor) "
+        "VALUES ( ${nuevoScan.id}, '${nuevoScan.tipo}', '${nuevoScan.valor}' )");
+    return res;
+  }
+
+  nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final res = await db.insert('Scans', nuevoScan.toJson());
+    return res;
   }
 }
