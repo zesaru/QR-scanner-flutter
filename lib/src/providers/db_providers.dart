2d985b15eaa9c4062efcd5a870fe3c1974c091db
Cesar Murillo <namiayumis@gmail.com>
2019-09-06 15:46:04 +0900

    obtener registros

--- lib/src/providers/db_providers.dart
+++ lib/src/providers/db_providers.dart
@@ -47,4 +47,29 @@ class DBProvider {
     final res = await db.insert('Scans', nuevoScan.toJson());
     return res;
   }
+
+  // SELECT - Obtener información
+  Future<ScanModel> getScanId(int id) async {
+    final db = await database;
+    final res = await db.query('Scans', where: 'id = ?', whereArgs: [id]);
+    return res.isNotEmpty ? ScanModel.fromJson(res.first) : null;
+  }
+
+  Future<List<ScanModel>> getTodosScans() async {
+    final db = await database;
+    final res = await db.query('Scans');
+
+    List<ScanModel> list =
+        res.isNotEmpty ? res.map((c) => ScanModel.fromJson(c)).toList() : [];
+    return list;
+  }
+
+  Future<List<ScanModel>> getScansPorTipo(String tipo) async {
+    final db = await database;
+    final res = await db.rawQuery("SELECT * FROM Scans WHERE tipo='$tipo'");
+
+    List<ScanModel> list =
+        res.isNotEmpty ? res.map((c) => ScanModel.fromJson(c)).toList() : [];
+    return list;
+  }
 }
