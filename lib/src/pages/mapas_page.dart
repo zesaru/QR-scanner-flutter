df2ab2c98bc5e1013e39008f5bb06a7757bc7b27
Cesar Murillo <namiayumis@gmail.com>
2019-09-07 11:56:54 +0900

    borrar registros de la base de datos

--- lib/src/pages/mapas_page.dart
+++ lib/src/pages/mapas_page.dart
@@ -20,13 +20,18 @@ class MapasPage extends StatelessWidget {
 
         return ListView.builder(
           itemCount: scans.length,
-          itemBuilder: (context, i) => ListTile(
-            leading: Icon(
-              Icons.cloud_queue,
-              color: Theme.of(context).primaryColor,
+          itemBuilder: (context, i) => Dismissible(
+            key: UniqueKey(),
+            background: Container(color: Colors.red),
+            onDismissed: (direction) => DBProvider.db.deleteScan(scans[i].id),
+            child: ListTile(
+              leading: Icon(
+                Icons.cloud_queue,
+                color: Theme.of(context).primaryColor,
+              ),
+              title: Text(scans[i].valor),
+              trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
             ),
-            title: Text(scans[i].valor),
-            trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
           ),
         );
       },
